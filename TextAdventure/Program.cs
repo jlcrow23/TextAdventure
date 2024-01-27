85f6bce7ea39d8e902a3762d3cfe24f833f296df
jlcrow23 <97205162+jlcrow23@users.noreply.github.com>
2024-01-26 20:52:23 -0700

    Stylize program

--- TextAdventure/Program.cs
+++ TextAdventure/Program.cs
@@ -8,11 +8,14 @@ namespace TextAdventure
         {
             Console.Clear();
             // Print backstory to user
-            Console.WriteLine("Welcome to the Haunted Mansion");
+            Console.WriteLine("************************************");
+            Console.WriteLine("  Welcome to the Haunted Mansion");
+            Console.WriteLine("************************************");
             Console.WriteLine("You have just inherited a mansion from a distant relative.");
-            Console.WriteLine("Now that you are the new owner you decide to check it out.");
+            Console.WriteLine("Now that you are the new owner, you decide to check it out.");
             Console.WriteLine("The house is dated, creepy, and falling apart. You walk in the front door.");
-            Console.WriteLine("");
+            Console.WriteLine();
+            System.Threading.Thread.Sleep(3000);
             Console.WriteLine("Do you want to enter the living room or the dining room?");
 
             // Prompt the user for choice
@@ -25,6 +28,7 @@ namespace TextAdventure
                 Console.WriteLine("You chose to go into the living room.");
                 Console.WriteLine("As you walk in, you see a sleeping pitbull guarding some gold jewelry.");
                 Console.WriteLine("");
+                System.Threading.Thread.Sleep(3000);
                 Console.WriteLine("Do you want to steal the jewelry?");
 
                 // Prompt for choice
@@ -64,10 +68,17 @@ namespace TextAdventure
                 else if(vaseChoice == "no")
                 {
                     Console.WriteLine("You decide not to open the vase.");
-                    Console.WriteLine("As you turn to leave you hear a cracking sound soming from the corner.");
-                    
+                    Console.WriteLine("As you turn to leave you hear a cracking sound coming from the corner.");
+                    System.Threading.Thread.Sleep(3000);
+                    Console.Clear();
                     Console.WriteLine("A dark figure with glowing red eyes launches at you, knocking you unconcious.");
-                    Console.WriteLine("You wake up in your bed. It was all a dream.... Or was it?");
+                    System.Threading.Thread.Sleep(3000);
+                    Console.WriteLine("You wake up in your bed.");
+                    System.Threading.Thread.Sleep(3000);
+                    Console.WriteLine("It was all a dream....");
+                    System.Threading.Thread.Sleep(3000);
+                    Console.WriteLine("Or was it?");
+                    System.Threading.Thread.Sleep(3000);
                 }
                 else
                 {
